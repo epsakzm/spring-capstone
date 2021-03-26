d6c2ab437d31246dda09caaad1af9a1a697b237d
박형우 <parkhw0614@gmail.com>
2021-03-26 17:12:20 +0900

    initial commit

---
+++ src/main/java/project/capstone/fick/FickApplication.java
@@ -0,0 +1,13 @@
+package project.capstone.fick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FickApplication.class, args);
+	}
+
+}
