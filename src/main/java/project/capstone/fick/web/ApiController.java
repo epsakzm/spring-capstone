731a1d1ec63c788837daa70d99a95cb4bfe6830b
nonoaa <nonoaa1@naver.com>
2021-03-28 18:47:00 +0900

    init

---
+++ src/main/java/project/capstone/fick/web/ApiController.java
@@ -0,0 +1,13 @@
+package project.capstone.fick.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import project.capstone.fick.service.SimpleService;
+
+@RequiredArgsConstructor
+@RestController
+public class ApiController {
+
+}
--- src/main/java/project/capstone/fick/web/dto/crack/CrackRequestDto.java
+++ src/main/java/project/capstone/fick/web/dto/crack/CrackRequestDto.java
@@ -1,4 +1,18 @@
 package project.capstone.fick.web.dto.crack;
 
+enum Hazard{
+    LOW, MIDDLE, HIGH
+}
+
 public class CrackRequestDto {
+    private String crackId;
+    private String image;
+    private float x;
+    private float y;
+    private float z;
+    private float width;
+    private Hazard hazard;
+    private String comment;
+    private boolean check;
+    private String buildingId;
 }
