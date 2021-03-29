3e7e82f32b1848d303c35e3ef409b7f1ba55aaae
nonoaa <nonoaa1@naver.com>
2021-03-29 16:53:55 +0900

    변경사항 저장

---
+++ src/main/java/project/capstone/fick/web/UserApiController.java
@@ -0,0 +1,21 @@
+package project.capstone.fick.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.capstone.fick.service.SimpleService;
+import project.capstone.fick.web.dto.user.UserLoginRequestDto;
+
+@RequiredArgsConstructor
+@RestController
+public class UserApiController {
+
+    @GetMapping("/api/user")
+    public ResponseEntity<UserLoginRequestDto> loginRequest(@RequestParam String name, @RequestParam Integer UID) {
+        UserLoginRequestDto dto = new UserLoginRequestDto();
+        dto.setName(name);
+        dto.setUID(UID);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+}
--- src/main/java/project/capstone/fick/web/dto/user/UserRequestDto.java
+++
@@ -1,4 +0,0 @@
-package project.capstone.fick.web.dto.user;
-
-public class UserRequestDto {
-}
