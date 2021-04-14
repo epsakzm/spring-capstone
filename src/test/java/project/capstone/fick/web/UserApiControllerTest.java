b3cce5b6d1db264cd9ac8a472915252629b05147
박형우 <parkhw0614@gmail.com>
2021-04-14 22:25:28 +0900

    Test ObjectMapper

---
+++ src/test/java/project/capstone/fick/web/UserApiControllerTest.java
@@ -0,0 +1,28 @@
+package project.capstone.fick.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import project.capstone.fick.web.dto.user.UserResponseDto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserApiControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Test
+	public void ObjectMapper_to_UserId() throws Exception {
+		System.out.println(objectMapper.writeValueAsString(new UserResponseDto.UserId(1L)));
+	}
+}
