a24789f14fa88e5ec1c405a915be187d2600edc7
박형우 <parkhw0614@gmail.com>
2021-05-13 18:20:22 +0900

    테스트 추가

--- src/test/java/project/capstone/fick/domain/structure/StructureRepositoryTest.java
+++ src/test/java/project/capstone/fick/domain/structure/StructureRepositoryTest.java
@@ -5,23 +5,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import project.capstone.fick.domain.project.Project;
+import project.capstone.fick.domain.project.ProjectRepository;
 
 import java.net.URI;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class StructureRepositoryTest {
 
 	@Autowired
 	private MockMvc mvc;
+
+	@Autowired
+	private StructureRepository repository;
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
 
 	@Test
 	public void readStructureTest() throws Exception{
@@ -37,5 +50,41 @@ class StructureRepositoryTest {
 
 		System.out.println("====================getBody================");
 		System.out.println(forEntity.getBody());
+	}
+
+	@Test
+	public void 특정ID_structure_조회() {
+		URI uri = UriComponentsBuilder.fromUriString("http://localhost:" + port)
+			.path("/api/v1/structure/1")
+			.build()
+			.toUri();
+		ResponseEntity<String> response = testRestTemplate.getForEntity(uri, String.class);
+
+		System.out.println("port : " + port);
+		System.out.println(response);
+	}
+
+	@Test
+	public void selectAllStructure() {
+		repository.findAll().stream().map(Structure::getId).forEach(System.out::println);
+		Optional<Structure> byId = repository.findById(1L);
+		Optional<Structure> structureByJoinFetch = repository.findStructureByJoinFetch(1L);
+
+		System.out.println("==========byId=============");
+		System.out.println(byId.get().getId());
+		System.out.println("==========byfetch=============");
+		System.out.println(structureByJoinFetch.get().getId());
+	}
+
+	@Autowired
+	private ProjectRepository projectRepository;
+
+	@Test
+	public void select() {
+		Optional<Project> byId = projectRepository.findById(1L);
+		Optional<Project> projectByJoinFetch = projectRepository.findProjectByJoinFetch(1L);
+
+		System.out.println(byId);
+		System.out.println(projectByJoinFetch);
 	}
 }
