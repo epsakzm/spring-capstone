7072b407bb13f64cbc6f2fcc1c8f460d1e3f2e58
박형우 <parkhw0614@gmail.com>
2021-05-19 11:12:28 +0900

    tc

--- src/main/java/project/capstone/fick/service/xlsx/XlsxService.java
+++ src/main/java/project/capstone/fick/service/xlsx/XlsxService.java
@@ -14,6 +14,7 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
@@ -68,8 +69,11 @@ public class XlsxService {
 			outputStream.flush();
 			outputStream.close();
 		} catch (Exception e) {
-			System.out.println("message");
-			e.printStackTrace();
+			try {
+				response.getWriter().write(e.getMessage());
+			} catch (IOException ioe) {
+				e.printStackTrace();
+			}
 		}
 		return ResponseEntity.ok().build();
 	}
